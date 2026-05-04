3e0afb5069f51241044fbccf9082525db84d316c
youssef wael <207546198+youssefwael1010@users.noreply.github.com>
2026-05-04 22:02:37 +0300

    Update models.py
    Added configurations for various machine learning models including RandomForest, Logistic Regression, Decision Tree, KNN, and SVM.

--- Final Pipeline/models.py
+++ Final Pipeline/models.py
@@ -1,1 +1,40 @@
+# =========================
+#  Models
+# =========================
+models_config = {
+    "RandomForest": {
+        "model": RandomForestClassifier(random_state=42,max_features='sqrt',class_weight='balanced',random_state=42,n_jobs=-1),
+        "params": {
+            "model__n_estimators": [200, 300, 400],
+            "model__max_depth": [15, 20, None],
+            "model__min_samples_leaf": [1, 2, 3]
+        }
+    },
 
+    "Logistic": {
+        "model": LogisticRegression(max_iter=1000, class_weight='balanced'),
+        "params": {
+            "model__C": [0.01, 0.1, 1, 10, 100]
+        }
+    },
+
+    "DecisionTree": {
+        "model": DecisionTreeClassifier(random_state=42, criterion='entropy'),
+        "params": {
+            "model__max_depth": [8, 10, 12],
+            "model__min_samples_leaf": [1, 3, 5],
+            "model__min_samples_split" : [2, 5, 10]
+        }
+    },
+    
+    "KNN": {
+        "model": KNeighborsClassifier(weights='distance'),
+        "params": {
+            "model__n_neighbors": [1, 30, 2]
+        }
+    },
+    
+    "SVM": {
+        "model": SVC(kernel='rbf',gamma=0.01, random_state=42 ,C=30,class_weight={0:1,1:2})
+    }
+}    
